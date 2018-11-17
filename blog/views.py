30c7705120392e2858b6fbd95c869b1d6de866de
mussaiin <160107015@stu.sdu.edu.kz>
2018-11-17 14:03:13 +0600

    [feat] Added Delete function and Pagination

--- blog/views.py
+++ blog/views.py
@@ -2,14 +2,28 @@ from django.shortcuts import render, get_object_or_404, redirect
 from django.utils import timezone
 from .models import Post
 from .forms import PostForm
+from django.core.paginator import EmptyPage, PageNotAnInteger, Paginator
+
 
 def post_list(request):
     posts = Post.objects.all().order_by('published_date')
-    return render(request, 'blog/post_list.html', {'posts': posts})
+    page = request.GET.get('page', 1)
+
+    paginator = Paginator(posts, 2)
+    try:
+        post = paginator.page(page)
+    except PageNotAnInteger:
+        post = paginator.page(1)
+    except EmptyPage:
+        post = paginator.page(paginator.num_pages)
+
+    return render(request, 'blog/post_list.html', {'posts': post})
+
 
 def post_detail(request, pk):
     post = get_object_or_404(Post, pk=pk)
     return render(request, 'blog/post_detail.html', {'post': post})
+
 
 def post_new(request):
     if request.method == "POST":
@@ -24,6 +38,7 @@ def post_new(request):
         form = PostForm()
     return render(request, 'blog/post_edit.html', {'form': form})
 
+
 def post_edit(request, pk):
     post = get_object_or_404(Post, pk=pk)
     if request.method == "POST":
@@ -37,3 +52,9 @@ def post_edit(request, pk):
     else:
         form = PostForm(instance=post)
     return render(request, 'blog/post_edit.html', {'form': form})
+
+
+def post_delete(request, pk):
+    Post.objects.filter(pk=pk).delete()
+    posts = Post.objects.all().order_by('published_date')
+    return render(request, 'blog/post_list.html', {'posts': posts})
